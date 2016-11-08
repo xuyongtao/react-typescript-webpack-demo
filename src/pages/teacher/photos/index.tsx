808f9fac74942408b0391c0151f0e3bbfd6e2e6c
xuyongtao <xuyongtao2009@gmail.com>
2016-11-08 23:19:25 +0800

    相册加react-lazyload和react-swipeable-views

--- src/pages/teacher/photos/index.tsx
+++ src/pages/teacher/photos/index.tsx
@@ -2,257 +2,178 @@ import "./index.less";
 import * as React from "react";
 import { render, findDOMNode } from "react-dom";
 import { TouchEvent, UIEvent } from "react";
+import SwipeableViews from "react-swipeable-views";
+import LazyLoad from "react-lazyload";
+// import AutoPlay from "react-swipeable-views/lib/autoPlay";
 
-class SwiperSlide extends React.Component<any, any> {
-    constructor(props: any, context: any) {
-        super(props, context);
+// const test = AutoPlay(SwipeableViews);
+// console.log(SwipeableViews);
+// console.log(AutoPlay);
 
-    }
 
-    componentDidMount() {
+import * as Lodash from "lodash";
 
-    }
-
-    render() {
-        let slideStyle = {
-            width: this.props.width,
-            marginLeft: this.props.index === 0 ? 0 : this.props.grapWidth,
-            backgroundImage: `url(${require(this.props.picSrc)})`,
-        }
-
-        return (
-            <li style={ slideStyle }></li>
-        )
-    }
+interface PhotosSwiperProps {
+    hidden: boolean,
+    maxSwiperWidth: number,
+    pics: string[],
+    index: number,
+    closeHandler: Function,
+}
+interface PhotosSwiperStates {
+    swiperWidth: number,
+    swiperHeight: number,
 }
 
-class SwiperPaginationBullet extends React.Component<any, any> {
-    constructor(props: any, context: any) {
-        super(props, context);
+class PhotosSwiper extends React.Component<PhotosSwiperProps, PhotosSwiperStates> {
+    // state: SwiperPhotosStates;
 
-    }
-
-    clickHandler(index: number) {
-        this.props.clickHandler(index);
-    }
-
-    render() {
-        return (
-            <span
-                className={ "swiper-pagination-bullet" + (this.props.active ? " swiper-pagination-bullet-active" : "") }
-                onClick={ this.clickHandler.bind(this, this.props.index) }
-                ></span>
-        )
-    }
-}
-
-class Swiper extends React.Component<any, any> {
-    constructor(props: any, context: any) {
+    constructor(props: PhotosSwiperProps, context: PhotosSwiperStates) {
         super(props, context);
 
         this.state = {
-            current: this.props.defaultCurrent || 1,
-            startClientX: 0,
-            endClientX: 0,
-            // swiperWidth: this.props.defaultSwiperWidth || 375,
             swiperWidth: 0,
-            dragLen: 0,
-        }
-
-    }
-
-    pre() {
-        let index = (this.state.current - 1) || 1;
-
-        this.setState({
-            current: index,
-            dragLen: - (this.state.swiperWidth + this.props.grapWidth) * (index - 1),
-        })
-    }
-
-    next() {
-        let index = ((this.state.current + 1) > this.props.pics.length ? this.state.current : (this.state.current + 1));
-
-        this.setState({
-            current: index,
-            dragLen: - (this.state.swiperWidth + this.props.grapWidth) * (index - 1),
-        })
-    }
-
-    go(index: number) {
-        this.setState({
-            current: index,
-            dragLen: - (this.state.swiperWidth + this.props.grapWidth) * (index - 1),
-        })
-    }
-
-    touchMoveHandler(event: TouchEvent) {
-        let touches = event.touches;
-        let picsCount = this.props.pics.length;
-        let dragLen = touches[0].clientX - this.state.startClientX;
-
-        if (
-            (this.state.current === 1 && dragLen > 0) ||
-            (this.state.current === picsCount && dragLen < 0)
-        ) {// 如果是第一张或者最后一张则不给拖拽
-            return;
-        }
-
-        this.setState({
-            dragLen: dragLen - (this.state.current - 1) * this.state.swiperWidth,
-        })
-    }
-
-    touchStartHandler(event: TouchEvent) {
-        let touches = event.touches;
-
-        this.setState({
-            startClientX: touches[0].clientX,
-        })
-    }
-
-    touchEndHandler() {
-        let dragLen = this.state.dragLen + (this.state.current - 1) * this.state.swiperWidth;
-        let plaitLen = this.state.swiperWidth / 3;
-        let direction = dragLen > 0 ? -1 : 1;
-
-        if (direction === 1 && dragLen < - plaitLen) {
-            this.next();
-        } else if (direction === -1 && dragLen > plaitLen) {
-            this.pre();
-        } else {
-            this.go(this.state.current);
+            swiperHeight: 0,
         }
     }
 
-    scrollHandler(e: UIEvent) {
-        e.stopPropagation();
+    getClientWH() {
+        let node = findDOMNode(this.refs['swiper']);
+
+        return {
+            W: node.clientWidth > this.props.maxSwiperWidth ? this.props.maxSwiperWidth : node.clientWidth,
+            H: node.clientHeight,
+        }
     }
 
-    getClientWidth() {
-        let clientWidth = findDOMNode(this.refs['swiper']).clientWidth;
-
-        return clientWidth > this.props.maxSwiperWidth ? this.props.maxSwiperWidth : clientWidth;
+    clickHandler() {
+        this.props.closeHandler();
     }
 
     componentDidMount() {
         this.setState({
-            swiperWidth: this.getClientWidth(),
+            swiperWidth: this.getClientWH().W,
+            swiperHeight: this.getClientWH().H,
         })
     }
 
     render() {
-        let swiperSlideProps = {
-            index: 0,
-            picSrc: '',
-            width: this.state.swiperWidth,
-            grapWidth: this.props.grapWidth,
-        };
+        console.log('index: ', this.props.index);
 
         let swiperStyle = this.state.swiperWidth ? { width: this.state.swiperWidth } : null;
-
-        let swiperSlideContainer = {
-            marginLeft: this.state.dragLen || - (this.state.current - 1) * (this.state.swiperWidth + this.props.grapWidth),
-            width: this.props.pics.length * (this.state.swiperWidth + this.props.grapWidth) - this.props.grapWidth,
+        let slideStyle = {
+            height: "100%",
+            width: "100%",
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+        };
+        let swipeableViewsProps = {
+            resistance: true,
+            index: this.props.index,
         };
 
         return (
-            <div>
-                <div style={ {
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    backgroundColor: '#000',
-                } }></div>
-                <div
-                    style={ swiperStyle }
-                    id="swiper"
-                    ref="swiper"
-                    onTouchStart={ this.touchStartHandler.bind(this) }
-                    onTouchEnd={ this.touchEndHandler.bind(this) }
-                    onTouchMove={ this.touchMoveHandler.bind(this) }
-                    onScroll={ this.scrollHandler.bind(this) }
-                    >
-                    <ul style={ swiperSlideContainer }>
-                        { this.props.pics.map((pic: string, index: number) => {
-                            swiperSlideProps.picSrc = pic;
-                            swiperSlideProps.index = index;
+            <div id="swiper" ref="swiper" style={ swiperStyle } hidden={ this.props.hidden } onClick={ this.clickHandler.bind(this) }>
+                <SwipeableViews { ...swipeableViewsProps }>
+                    { this.props.pics.map((pic: string, index: number) => {
+                        return (
+                            <div key={ index } style={ Lodash.assign({}, slideStyle, { backgroundImage: `url(${require(pic)})` }) } ></div>
+                        )
+                    }) }
+                </SwipeableViews>
 
-                            return (
-                                <SwiperSlide key={ index } { ...swiperSlideProps } />
-                            )
-                        }) }
-                    </ul>
-                    <div className="swiper-pagination">
-                        { this.props.pics.map((pic: string, index: number) => {
-                            let props = {
-                                key: index,
-                                index,
-                                active: this.state.current === index + 1,
-                                clickHandler: this.go.bind(this),
-                            };
-
-                            return (
-                                <SwiperPaginationBullet { ...props } />
-                            )
-                        }) }
-                    </div>
-                </div>
             </div>
-
         )
     }
 }
 
-export default class TeacherPhotos extends React.Component<any, any> {
-    constructor() {
-        super();
+interface TeacherPhotosProps {
+    params: any,
+}
+interface TeacherPhotosStates {
+    hiddenSwiper?: boolean;
+    pics?: string[];
+    swiperIndex?: number;
+}
+
+export default class TeacherPhotos extends React.Component<TeacherPhotosProps, TeacherPhotosStates> {
+    constructor(props: TeacherPhotosProps, context: TeacherPhotosStates) {
+        super(props, context);
 
         this.state = {
-            pics: ["./pic1.jpeg", "./pic2.jpeg", "./pic3.jpeg", "./pic4.jpeg", "./pic5.jpeg"],
-        }
+            hiddenSwiper: false,
+            pics: [],
+            swiperIndex: 1,
+        };
+    }
+
+    showPhotosSwiper(index: number) {
+        this.setState({
+            hiddenSwiper: false,
+            swiperIndex: index,
+        })
+    }
+
+    hidePhotosSwiper() {
+        this.setState({
+            hiddenSwiper: true,
+        })
     }
 
     componentDidMount() {
-
+        // 通过请求获取图片的链接地址
+        this.setState({
+            pics: ["./pic2.jpeg", "./pic3.jpeg", "./pic1.jpeg", "./pic4.jpeg", "./pic6.png", "./pic5.jpeg", "./pic7.png", "./pic8.png", "./pic10.jpeg", "./pic9.png", "./pic2.jpeg", "./pic1.jpeg", "./pic5.jpeg", "./pic3.jpeg", "./pic4.jpeg", "./pic6.png", "./pic8.png", "./pic9.png", "./pic10.jpeg", "./pic7.png"],
+        })
     }
 
     render() {
-        console.log('构建相册', this.refs["gallery"]);
         let swiperProps = {
-            pics: ["./pic2.jpeg", "./pic1.jpeg", "./pic3.jpeg", "./pic4.jpeg", "./pic5.jpeg"],
-            grapWidth: 8,
+            hidden: this.state.hiddenSwiper,
             maxSwiperWidth: 640,
-            defaultCurrent: 1,
-        }
-
+            pics: this.state.pics,
+            index: this.state.swiperIndex,
+            closeHandler: this.hidePhotosSwiper.bind(this),
+        };
+        let lazyloadProps = {
+            height: 200,
+        };
+        let pics = {
+            left: Lodash.reject(this.state.pics, (pic, index) => { return index / 2 === 0 }),
+            right: Lodash.reject(this.state.pics, (pic, index) => { return index / 2 === 1 }),
+        };
 
         return (
             <div id="gallery-wall">
-                <Swiper { ...swiperProps } />
+                <PhotosSwiper { ...swiperProps }/>
+
                 <div className="gallery-wall-left">
-                    <div className="gallery-wall-item">
-                        <img src={ require("./pic1.jpeg") } alt=""/>
-                    </div>
-                    <div className="gallery-wall-item">
-                        <img src={ require("./pic2.jpeg") } alt=""/>
-                    </div>
-                    <div className="gallery-wall-item">
-                        <img src={ require("./pic5.jpeg") } alt=""/>
-                    </div>
+                    { pics.left.map((pic, index) => {
+                        return (
+                            <div key={ index * 2 + 1 } className="gallery-wall-item" onClick={ this.showPhotosSwiper.bind(this, index * 2 + 1) }>
+                                <LazyLoad { ...lazyloadProps }>
+                                    <img src={ require(pic) } alt={ `图片${index * 2}` }/>
+                                </LazyLoad>
+                            </div>
+                        )
+                    }) }
                 </div>
                 <div className="gallery-wall-right">
-                    <div className="gallery-wall-item">
-                        <img src={ require("./pic3.jpeg") } alt=""/>
-                    </div>
-                    <div className="gallery-wall-item">
-                        <img src={ require("./pic4.jpeg") } alt=""/>
-                    </div>
+                    { pics.right.map((pic, index) => {
+                        return (
+                            <div key={ index * 2 } className="gallery-wall-item" onClick={ this.showPhotosSwiper.bind(this, index * 2) }>
+                                <LazyLoad { ...lazyloadProps }>
+                                    <img src={ require(pic) } alt={ `图片${index * 2 + 1}` }/>
+                                </LazyLoad>
+                            </div>
+                        )
+                    }) }
                 </div>
             </div>
         )
     }
 }
+
+
 
