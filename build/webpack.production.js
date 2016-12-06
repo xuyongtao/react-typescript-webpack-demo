f78464e3e9d71e89f13770dba21a1a4ee552b658
xuyongtao <xuyongtao2009@gmail.com>
2016-12-06 21:28:38 +0800

    解决发布到生成环境报的warning

--- build/webpack.production.js
+++ build/webpack.production.js
@@ -2,6 +2,15 @@ var webpack = require('webpack');
 var config = require('./webpack.config');
 
 // 压缩 js、css
+console.log('webpack working');
+// 处理warning http://stackoverflow.com/questions/30030031/passing-environment-dependent-variables-in-webpack
+config.plugins.push(
+    new webpack.DefinePlugin({
+        "process.env": {
+            NODE_ENV: JSON.stringify("production")
+        }
+    })
+)
 config.plugins.push(
     new webpack.optimize.UglifyJsPlugin({
         compress: {
@@ -12,6 +21,11 @@ config.plugins.push(
 
 webpack(config, function (err, status) {
     if (err) {
-        console.log(err);
+        console.log('fail! ', err);
+    } else {
+        console.log('done!');
     }
 });
+
+// 针对react router，nginx需要做的配置
+// http://www.59m59s.com/blog/diao-zheng-nginxzheng-que-fu-wu-react-routerying-yong/
