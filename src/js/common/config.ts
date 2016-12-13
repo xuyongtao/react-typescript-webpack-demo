ef9f7af4d836f67282096c8e5f2a6f6ce111f1a9
xuyongtao <xuyongtao2009@gmail.com>
2016-12-13 20:29:23 +0800

    no message

--- src/js/common/config.ts
+++ src/js/common/config.ts
@@ -2,10 +2,10 @@ export const defaultAvatar = require("../../img/default-avatar.png");
 
 export const defaultCourseCover = require("../../img/default-course-cover.png");
 
-export const publicPath = "http://192.168.2.55:8080/";
-// export const publicPath = "http://m.qmin91.com/";
-// export const apiHost = "http://qmin91.com/";
-export const apiHost = "http://192.168.2.55:8080/";
+// export const publicPath = "http://192.168.2.55:8080/";
+export const publicPath = "http://m.qmin91.com/";
+export const apiHost = "http://qmin91.com/";
+// export const apiHost = "http://192.168.2.55:8080/";
 
 export const apis = {
     getIndexRoleList: `${apiHost}apis/mobile/getIndexRoleList`,
